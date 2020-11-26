6e096d90162d11c680c2b30b60276e2f292d5063
maks <a@a.a>
2020-11-26 11:19:00 +0300

    slider

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-let active = 3;
+let active = 0;
 let mainGirl= document.querySelectorAll('.main__girl');
 let list = document.querySelectorAll('.menu__list');
-let listLength = list.length;
-
+let mainGirlImages = document.querySelectorAll('.main__girl__images img');
+let listLength = mainGirlImages.length;
 let ul = document.querySelector('ul');
 
+const menuListImages = document.querySelectorAll('.menu__list img');
 
+
+// map forEach filter reduce(с аккумулятором)
+// слева массив
+// справа функция
+//  massiv.forEach( (e,i,m)=>{ e.addEventListener("click", ()=>{console.log("click")})}  )
+
+const changeActive = (to)=>{
+
+    mainGirlImages[active].style.opacity = '0';
+    mainGirlImages[to].style.opacity = '1';
+    menuListImages[active].classList.remove('active');
+    menuListImages[to].classList.add('active');
+
+    active = to;
+
+}
+
+
+
+menuListImages.forEach((e, i) =>{
+    e.addEventListener('click', (event)=> {
+        
+    changeActive(i);
+    
+    })
+})
 let nextArrow = document.querySelector('.next-arrow');
 nextArrow.addEventListener('click', ()=>{
     
     let next =  (active + 1 ) % listLength;
-    let position = next * 400;
 
-    console.log(`${position}px;`)
-    ul.style.left = `-${position}px`
-
-    // console.log(`next is ${next} `)
-    // list[active].classList.remove('active');
-    // list[next].classList.add('active');
-    
-    active = next;
+    changeActive(next);
 
 })
 
+
+
+const prevArrow = document.querySelector('.prev-arrow');
+prevArrow.addEventListener('click', () =>{
+    const  prev = (active - 1 + listLength) %  listLength;
+    changeActive(prev);
+
+} )
 
 
 
